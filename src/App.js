d2643cc961bd7ec0ac337c590bd74196c4a1c923
FrancescoCataletto <catalettofrancesco@gmail.com>
2022-08-04 16:14:10 +0200

    first react app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(props) {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          My first React App
         </p>
+        <p>{props.name}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
