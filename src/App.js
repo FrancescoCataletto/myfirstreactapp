334ed31d040004d2fd91cbddea78b8f4d1174632
FrancescoCataletto <catalettofrancesco@gmail.com>
2022-08-05 22:42:09 +0200

    created a product component to try to use Props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Product from "./Product";
 
-function App(props) {
+function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          My first React App
-        </p>
-        <p>{props.name}</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>AMAZON</h1>
+
+      <Product name="Amazon Echo" description="Your AI assistant" price={59.99}/>
     </div>
   );
 }
