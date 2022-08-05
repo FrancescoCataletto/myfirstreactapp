8a5ad6ba8131b86dc1e2fe9c381aa73e60998a90
FrancescoCataletto <catalettofrancesco@gmail.com>
2022-08-05 22:48:57 +0200

    created different products in App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,21 @@ function App() {
     <div>
       <h1>AMAZON</h1>
 
-      <Product name="Amazon Echo" description="Your AI assistant" price={59.99}/>
+      <Product 
+      name="Amazon Echo" 
+      description="Your AI assistant" 
+      price={59.99}
+      />
+      <Product 
+      name="Google Home" 
+      description="Your AI assistant" 
+      price={49.99}
+      />
+      <Product 
+      name="Alexa" 
+      description="Your AI assistant" 
+      price={79.99}
+      />
     </div>
   );
 }
