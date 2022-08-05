6d13319f06e25cd7a7253172ee576d11ab3cea93
FrancescoCataletto <catalettofrancesco@gmail.com>
2022-08-05 22:45:20 +0200

    stated props in the Product.js

--- src/Product.js
+++ src/Product.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 function Product(props) {
   return (
-    <div>Product</div>
+    <div>
+        <h2>{props.name}</h2>
+        <h2>{props.description}</h2>
+        <h3>${props.price}</h3>
+    </div>
   )
 }
 
