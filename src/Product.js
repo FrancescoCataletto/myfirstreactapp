91ff1960158381a50e024cd9cd3e3066962867eb
FrancescoCataletto <catalettofrancesco@gmail.com>
2022-08-05 22:52:30 +0200

    changed from props. to name of each property

--- src/Product.js
+++ src/Product.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-function Product(props) {
+function Product({name, description, price}) {
   return (
     <div>
-        <h2>{props.name}</h2>
-        <h2>{props.description}</h2>
-        <h3>${props.price}</h3>
+        <h2>{name}</h2>
+        <h2>{description}</h2>
+        <h3>${price}</h3>
     </div>
   )
 }
